3aa51862a5d54bc159262ff3b3850bfe2ed3d72b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-09-10 18:55:50 -0400

    Intial Commit

---
+++ Assets/_Scripts/HelloWorldPluginWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class HelloWorldPluginWrapper : MonoBehaviour {
+
+	const string DLL_NAME = "HelloWorldPlugin";
+
+	[DllImport(DLL_NAME)]
+	private static extern int Add(int first, int second);
+
+	[DllImport(DLL_NAME)]
+	private static extern System.IntPtr Sayhello();
+	
+	[DllImport(DLL_NAME)]
+	private static extern  void SetGreeting(string greeting);
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.H)) {
+			print(Add(7, 3));
+			SetGreeting("Hello, World!");
+
+			string message = Marshal.PtrToStringAnsi(Sayhello());
+			print(message);
+		}
+
+		if(Input.GetKeyDown(KeyCode.G)) {
+			SetGreeting("Goodbye!");
+			
+			string message = Marshal.PtrToStringAnsi(Sayhello());
+			print(message);
+		}
+	}
+}
